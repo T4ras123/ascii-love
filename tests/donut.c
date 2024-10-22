c79322c8a056125c0084ffb4c82b2f6f08f2d3a3
T4ras123 <vovatara123@gmail.com>
2024-10-22 14:59:32 +0400

    Adjust screen width for improved rendering calculations

--- tests/donut.c
+++ tests/donut.c
@@ -12,7 +12,7 @@ const float phi_spacing = 0.02;
 const float R1 = 1;
 const float R2 = 2;
 const float K2 = 5;
-const int screen_width = 50;
+const int screen_width = 40;
 const float K1 = screen_width * K2 * 3 / (8 * (R1 + R2));
 
 void render_frame(float A, float B) {
